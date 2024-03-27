4c343f3cbd74c197994a36066ed38fa611df40fb
YdVynex <Yongduan123@gmail.com>
2024-03-27 11:53:22 +0100

    Labs 26 Maart

--- src/main/java/h5/Main.java
+++ src/main/java/h5/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Bigger b = new Bigger(5, 6);
         int number = b.greatest();
-        Factorial f = new Factorial(3);
+        System.out.println(number);
+
+        Factorial f = new Factorial(5);
         f.calculate();
     }
 }
