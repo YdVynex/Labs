9a6d27c6bd465d04cf6fbb56bf9ece1c2c8997bc
YdVynex <165004370+YdVynex@users.noreply.github.com>
2024-03-28 14:25:02 +0100

    Labs 26 Maart

--- src/main/java/h7/model/Account.java
+++ src/main/java/h7/model/Account.java
@@ -3,15 +3,47 @@ package h7.model;
 import java.util.ArrayList;
 
 public class Account {
-    public static String name;
-    public static String residence;
-    public int ID;
+    private final String name;
+    private final String residence;
+    public final int ID;
+    private final String gender;
+
     public ArrayList<Integer> listOrders = new ArrayList<Integer>();
-    public Account(String naam, String plaats){
+    public Account(String naam, String plaats, String Gender){
         name = naam;
         residence = plaats;
+        gender = Gender;
         ID = (int) ((double) Math.random() * 100000);
         System.out.println("Order ID created:");
         System.out.println(ID);
+    }
+    @Override
+    public String toString(){
+        System.out.println(name + "(" + residence + ")" + " is a " + gender);
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object a){
+        if(a == this){return true;}
+
+        if(a instanceof Account acc) {
+            if (acc.name.equals(this.name) && acc.gender == this.gender && acc.ID == this.ID) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        double hashCodeName = name.hashCode();
+        double hashCodeGender = gender.hashCode();
+        double hashCodeTotal = hashCodeGender * hashCodeName;
+        System.out.println(hashCodeTotal);
+        return 0;
     }
 }
