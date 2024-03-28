f4821092a94f6304d005d5176bf4153608ac4519
YdVynex <165004370+YdVynex@users.noreply.github.com>
2024-03-28 13:07:45 +0100

    Labs 26 Maart

--- src/main/java/h7/repos/AccountRepo.java
+++ src/main/java/h7/repos/AccountRepo.java
@@ -1,14 +1,16 @@
 package h7.repos;
 
 import h7.model.Account;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static h7.model.Item.itemList;
 
 
-public class AccountRepo {
+public class AccountRepo implements Iterable {
     public List<Account> accountList = new ArrayList<Account>();
 
     public void addAccount(Account x){
@@ -31,5 +33,19 @@ public class AccountRepo {
             System.out.println(b.listOrders);
         }
         }
+    }
+    @Override
+    public Iterator<Account> iterator() {
+        return new Iterator() {
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < accountList.size();
+            }
+            @Override
+            public Account next() {
+                return accountList.get(index++);
+            }
+        };
     }
 }
